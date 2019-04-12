38b7042bc51252bfecdfa25b4285389235dd7dc4
Bob Roberts <cardsfanbob@gmail.com>
2019-04-12 10:53:52 -0500

    Got most of the enums and types created

---
+++ app/graphql/types/vote_position_enum_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class VotePositionEnumType < Types::BaseEnum
+    description 'The vote position'
+
+    value 'yes', 'Yes vote'
+    value 'no', 'No vote' 
+    value 'not_voting', 'Not voting' 
+    value 'speaker', 'Speaker' 
+    value 'present', 'Present' 
+  end
+end
