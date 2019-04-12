6c20dd4e3bd79567b3700a9ae1e3363749cf8af9
Bob Roberts <cardsfanbob@gmail.com>
2019-04-12 15:39:40 -0500

    Bills query specs

--- spec/graphql/queries/bills_query_spec.rb
+++ spec/graphql/queries/bills_query_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 Rspec.describe Types::BillType, type: :request do
   let!(:bills) { create_list :bill, 5 }
 
-  it 'gets list of bills' do
+  it 'gets list of all_bills' do
     query =
       <<~GQL
           query {
@@ -19,6 +19,27 @@ Rspec.describe Types::BillType, type: :request do
     data = json.dig('data', 'allBills')
     expect(data.count).to eq 5
   end
+
+  it 'gets featured_bills' do
+    Bill.last.update(feature_state: 'featured')
+
+    query =
+      <<~GQL
+          query {
+            featuredBills {
+              id
+              number
+            }
+          }
+      GQL
+
+    post '/api/graphql', params: { query: query }
+    expect(response).to be_successful
+    data = json.dig('data', 'featuredBills')
+    expect(data.count).to eq Bill.special.count
+  end
+
+  it 'gets bills'
 
   it 'gets a bill' do
     query =
